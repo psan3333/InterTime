e8a7909fbf4509e4f91c1a883fd375f5f03a7556
psan3333 <psan3333@mail.ru>
2026-06-05 11:16:55 +0700

    chore: update project folder structure

--- app/components/icons/GridIcon.tsx
+++
@@ -1,6 +0,0 @@
-import { IoGridOutline } from "react-icons/io5";
-import { IconProps } from "../interfaces";
-
-export default function GridIcon(props: IconProps) {
-  return <IoGridOutline {...props} />;
-}
